dc55e36684f3b0a95f4fc299ec86ebbc23ead9a4
Onur Cil <onurcil08@gmail.com>
2018-05-09 01:55:01 +0300

    initial commit

---
+++ backend/src/main/java/com/onur/myapp/firstapp/Car.java
@@ -0,0 +1,45 @@
+package com.onur.myapp.firstapp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@ToString @EqualsAndHashCode
+public class Car {
+    @Id @GeneratedValue
+    private Long id;
+    private @NonNull String name;
+    
+    public Car() {
+    	
+    }
+	public Car(String name) {
+		this.name = name;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Car{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+	}
+    
+    
+}
